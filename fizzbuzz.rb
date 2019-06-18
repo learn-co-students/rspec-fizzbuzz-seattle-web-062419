f6143e73857ef83135b5eff52fe5caf7efc96272
evancarlsen <evan@evancarlsen.com>
2019-06-18 15:42:31 -0700

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
+require 'pry'
+def fizzbuzz(int)
+    str = ""
+    if int % 3 == 0
+        str += "Fizz"
+    end
+    if int % 5 == 0
+        str += "Buzz"
+    end
+    if str == ""
+        return nil
+    end
+    return str
+end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
